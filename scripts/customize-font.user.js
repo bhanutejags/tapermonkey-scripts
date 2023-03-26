ac04509599770c2c3f4e9fe260b96e03b4041710
bhanutejags <10769493+bhanutejags@users.noreply.github.com>
2023-03-26 13:23:43 -0500

    Increasing version.

--- scripts/customize-font.user.js
+++ scripts/customize-font.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Customize Font on Mozilla Firefox
 // @namespace    BTGS:Font
-// @version      0.1
+// @version      0.2
 // @description  Customizes website font to OpenSans as the default san-serif font and SauceCodePro Nerd Font as the monospace font.
 // @author       bhanutejags
 // @match        https://*/*
@@ -11,8 +11,6 @@
 // @exclude      *.*.gov/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        GM_addStyle
-// @downloadUrl  https://raw.githubusercontent.com/bhanutejags/tapermonkey-scripts/main/scripts/customize-font.user.js
-// @updateUrl    https://raw.githubusercontent.com/bhanutejags/tapermonkey-scripts/main/scripts/customize-font.user.js
 // ==/UserScript==
 GM_addStyle ( `
     body {
